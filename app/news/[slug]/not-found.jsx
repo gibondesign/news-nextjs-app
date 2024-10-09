8ca97a207b1327aa34a454edd6174abcbf33b03a
Gib-ON Design <gibonwroc@gmail.com>
2024-10-09 14:21:34 +0200

    dynamic news page and error handling

---
+++ app/news/[slug]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<div id="error">
+			<h1>Page not found</h1>
+            <h3>We could not find the requestet article</h3>
+		</div>
+	);
+}
---
+++ app/news/[slug]/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+
+export default function ArticlePage({ params }) {
+	const newsSlug = params.slug;
+	const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+	if (!newsItem){
+	  notFound()
+	}
+
+const newDate = new Date(newsItem.date)
+const formattedDate = newDate.toLocaleDateString('en-GB',{
+  day:'2-digit',
+  month: 'long',
+  year: 'numeric'
+})
+
+	return (
+		<article className='news-article'>
+			<header>
+				<img src={`/images/news/${newsItem.image}`} alt="" />
+				<h1>{newsItem.title}</h1>
+				<time datetime={newsItem.date}>{formattedDate}</time>
+			</header>
+      <p>{newsItem.content}</p>
+		</article>
+	);
+}
