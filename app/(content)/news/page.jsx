9aff61eaec94b844eb0f60c2245d3a17e47fb4e0
Gib-ON Design <gibonwroc@gmail.com>
2024-10-21 22:14:06 +0200

    Fetching data in the classic React way.

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,13 +1,50 @@
+"use client";
 import NewsList from "@/components/NewsList";
-import { getAllNews } from "@/lib/news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
-	const news = getAllNews();
+	const [news, setNews] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		async function fetchNews() {
+			setIsLoading(true);
+			setError(null)
+
+			try {
+				const response = await fetch(" http://localhost:8080/news");
+				
+				if (!response.ok) {
+					throw new Error("Failed to fetch news.");
+				}
+				const newsFetch = await response.json();
+				setNews(newsFetch)
+			} catch (err) {
+				setError(err.message)
+			} finally {
+				setIsLoading(false)
+			}
+		
+		}
+
+		fetchNews();
+	}, []);
+
+	let newsContent;
+
+	if (news) {
+		newsContent = <NewsList news={news} />;
+	}
+	console.log(news);
 
 	return (
 		<>
 			<h1>News Page</h1>
-			<NewsList news={news} />
+			{isLoading && <p>Fetching data....</p>}
+			{error && <p>{error}</p>}
+			{error}
+			{newsContent}
 		</>
 	);
 }
