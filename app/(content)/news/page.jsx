5b4b7be32a8413b81e8736e5c2eb62ca133c91da
Gib-ON Design <gibonwroc@gmail.com>
2024-10-22 09:46:23 +0200

    Server-side Data Fetching - the best option in Next.js.

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,50 +1,17 @@
-"use client";
 import NewsList from "@/components/NewsList";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-	const [news, setNews] = useState();
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
+export default async function NewsPage() {
+	const response = await fetch("http://localhost:8080/news");
 
-	useEffect(() => {
-		async function fetchNews() {
-			setIsLoading(true);
-			setError(null)
-
-			try {
-				const response = await fetch(" http://localhost:8080/news");
-				
-				if (!response.ok) {
-					throw new Error("Failed to fetch news.");
-				}
-				const newsFetch = await response.json();
-				setNews(newsFetch)
-			} catch (err) {
-				setError(err.message)
-			} finally {
-				setIsLoading(false)
-			}
-		
-		}
-
-		fetchNews();
-	}, []);
-
-	let newsContent;
-
-	if (news) {
-		newsContent = <NewsList news={news} />;
+	if (!response.ok) {
+		throw new Error("Faild to fetch news...");
 	}
-	console.log(news);
+	const news = await response.json();
 
 	return (
 		<>
 			<h1>News Page</h1>
-			{isLoading && <p>Fetching data....</p>}
-			{error && <p>{error}</p>}
-			{error}
-			{newsContent}
+			<NewsList news={news} />
 		</>
 	);
 }
